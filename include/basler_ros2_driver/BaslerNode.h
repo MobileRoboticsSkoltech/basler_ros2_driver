56939ce496ba05384aabe4679d703b9f54134727
Devilox96 <devilox96@yandex.ru>
2021-09-22 17:12:22 +0300

    Fixed some bugs, added fps option and yaml config

--- include/basler_ros2_driver/BaslerNode.h
+++ include/basler_ros2_driver/BaslerNode.h
@@ -10,10 +10,13 @@
 #include <image_transport/image_transport.hpp>
 #include <sensor_msgs/image_encodings.hpp>
 #include <camera_calibration_parsers/parse.hpp>
+#include <ament_index_cpp/get_package_share_directory.hpp>
 //-----------------------------//
 #include <pylon/usb/BaslerUsbInstantCamera.h>
 #include <pylon/usb/BaslerUsbInstantCamera.h>
 #include <pylon/PylonIncludes.h>
+//-----------------------------//
+#include <yaml-cpp/yaml.h>
 //-----------------------------//
 class BaslerNode : public rclcpp::Node {
 public:
@@ -25,16 +28,20 @@ public:
     void publishFrame();
     void startLoop();
 private:
-    Pylon::CBaslerUsbInstantCamera*     mCamera                     = nullptr;
-    Pylon::CImageFormatConverter        mFormatConverter;
-    Pylon::CPylonImage                  mPylonImage;
-    Pylon::CGrabResultPtr               mGrabResultPrt;
+    Pylon::CBaslerUsbInstantCamera*                                 mCamera                     = nullptr;
+    Pylon::CImageFormatConverter                                    mFormatConverter;
+    Pylon::CPylonImage                                              mPylonImage;
+    Pylon::CGrabResultPtr                                           mGrabResultPrt;
 
     //----------//
 
-    image_transport::CameraPublisher    mPublisher;
-    sensor_msgs::msg::CameraInfo        mCameraInfo;
-    std::string                         mCameraName                 = "BaslerCamera";
+    image_transport::CameraPublisher                                mPublisher;
+    sensor_msgs::msg::CameraInfo                                    mCameraInfo;
+    std::string                                                     mCameraName                 = "BaslerCamera";
+
+    std::chrono::time_point <std::chrono::system_clock>             mStart;
+    std::chrono::time_point <std::chrono::system_clock>             mEnd;
+    uint16_t                                                        mFPS                        = 30;
 };
 //-----------------------------//
 #endif //BASLER_ROS2_BASLERNODE_H
--- src/BaslerNode.cpp
+++ src/BaslerNode.cpp
@@ -10,13 +10,24 @@ BaslerNode::BaslerNode() : Node("Basler") {
 
     //----------//
 
+    std::string PackagePath = ament_index_cpp::get_package_share_directory("basler_ros2");
+
     mPublisher = image_transport::create_camera_publisher(this, "basler/image");
-    camera_calibration_parsers::readCalibration("config/basler_intrinsics_calibration.yaml", mCameraName, mCameraInfo);
+    camera_calibration_parsers::readCalibration(PackagePath + "/config/basler_intrinsics_calibration.yaml", mCameraName, mCameraInfo);
 
     //----------//
 
     mCamera -> Open();
     mCamera -> StartGrabbing(Pylon::GrabStrategy_LatestImageOnly);
+
+    //----------//
+
+    YAML::Node Config = YAML::LoadFile(PackagePath + "/config/basler_params.yaml");
+    mFPS = Config["fps"].as <int>();
+
+    //----------//
+
+    mStart = std::chrono::system_clock::now();
 }
 BaslerNode::~BaslerNode() {
     mCamera -> Close();
@@ -34,24 +45,30 @@ void BaslerNode::publishFrame() {
         mCamera -> RetrieveResult(5000, mGrabResultPrt, Pylon::TimeoutHandling_ThrowException);
 
         if (mGrabResultPrt -> GrabSucceeded()) {
-            mFormatConverter.Convert(mPylonImage, mGrabResultPrt);
+            mEnd = std::chrono::system_clock::now();
+            std::chrono::duration <double> Dur(mEnd - mStart);
 
-            const uint8_t* Buffer = reinterpret_cast <uint8_t*> (mPylonImage.GetBuffer());
-            const size_t Size = mGrabResultPrt -> GetImageSize() * 3;
+            if (Dur.count() > 1.0 / mFPS) {
+                mStart = std::chrono::system_clock::now();
+                mFormatConverter.Convert(mPylonImage, mGrabResultPrt);
 
-            sensor_msgs::msg::Image RawImg;
-            RawImg.header.stamp = this -> now();
-            RawImg.header.frame_id = "/basler";
-            RawImg.width = mGrabResultPrt -> GetWidth();
-            RawImg.height = mGrabResultPrt -> GetHeight();
-            RawImg.encoding = sensor_msgs::image_encodings::RGB8;
-            RawImg.step = RawImg.width * 3;
-            RawImg.data.assign(Buffer,Buffer + Size);
+                const uint8_t* Buffer = reinterpret_cast <uint8_t*> (mPylonImage.GetBuffer());
+                const size_t Size = mGrabResultPrt -> GetImageSize() * 3;
 
-            mCameraInfo.header.stamp = RawImg.header.stamp;
-            mCameraInfo.header.frame_id = RawImg.header.frame_id;
+                sensor_msgs::msg::Image RawImg;
+                RawImg.header.stamp = this -> now();
+                RawImg.header.frame_id = "/basler";
+                RawImg.width = mGrabResultPrt -> GetWidth();
+                RawImg.height = mGrabResultPrt -> GetHeight();
+                RawImg.encoding = sensor_msgs::image_encodings::RGB8;
+                RawImg.step = RawImg.width * 3;
+                RawImg.data.assign(Buffer,Buffer + Size);
 
-            mPublisher.publish(RawImg, mCameraInfo);
+                mCameraInfo.header.stamp = RawImg.header.stamp;
+                mCameraInfo.header.frame_id = RawImg.header.frame_id;
+
+                mPublisher.publish(RawImg, mCameraInfo);
+            }
         } else {
             std::cerr << "Runtime error" << std::endl;
         }
